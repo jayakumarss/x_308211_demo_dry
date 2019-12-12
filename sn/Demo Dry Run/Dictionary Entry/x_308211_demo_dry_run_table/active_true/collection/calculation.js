341d60ff895ab3233bdae30ca340ac89670e00f7
System Administrator <admin@example.com>
2019-12-12 09:16:05 -0500

    Changes by 'System Administrator' on 'Demo Dry Run - 1.0.0' -- Build #1

---
+++ sn/Demo Dry Run/Dictionary Entry/x_308211_demo_dry_run_table/active_true/collection/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : Demo Dry Run
+ * ClassName   : sys_dictionary
+ * Created On  : 2019-12-12 14:07:16
+ * Created By  : admin
+ * Updated On  : 2019-12-12 14:07:16
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=24f327e7db210010343f58b3ca961958
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
---
+++ sn/Demo Dry Run/Script Include/active_true/x_308211_demo_dry.DemoDryRun1.js
@@ -0,0 +1,35 @@
+/* 
+ * Application : Demo Dry Run
+ * ClassName   : sys_script_include
+ * Created On  : 2019-12-12 14:09:12
+ * Created By  : admin
+ * Updated On  : 2019-12-12 14:13:36
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=3204e7e7db210010343f58b3ca9619f9
+ */
+/**
+ * Class Description
+ * 
+ * @class 
+ * @author System Administrator [admin]
+ * @memberof x_308211_demo_dry.module:sys_script_include
+ */
+var DemoDryRun1 = Class.create();
+DemoDryRun1.prototype = /** @lends x_308211_demo_dry.module:sys_script_include.DemoDryRun1.prototype */ {
+    /**
+     * Description
+     * 
+     * @returns {undefined}
+     */
+    initialize: function() {
+    },
+	/**
+	 * Description
+	 * 
+	 * @returns {boolean} 
+	 */
+	executeTest: function(){
+		return true;
+	},
+	type: 'DemoDryRun1'
+};
